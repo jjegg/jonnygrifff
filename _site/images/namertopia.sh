2a02ba676b7047eb3276dc5788a3f2fa75cc7b17
jonny-wg2 <39831372+jonny-wg2@users.noreply.github.com>
2020-10-11 21:54:33 +0200

    add fall images

--- _site/images/namertopia.sh
+++ _site/images/namertopia.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# rename images
-# bash namertopia.sh path/to/my/images
+
+# RENAME IMAGES
+# bash namertopia.sh rename 2020-springioslo
+
+# RESIZE
+# bash namertopia.sh resize 2020-springioslo
+
 
 # global
 tmp_dir=$(mktemp -d -t namertopia-XX)
@@ -12,21 +17,23 @@ setup_mac () {
 }
 
 resize () { 
+    echo $tmp_dir
     echo "starting resizing"
-    
     echo "saving images to HQ directory"
-    mkdir $1/HQ && cp $1/*.jpg $1/HQ
+    mkdir $1/HQ 
+    cp -R $1/*.jpg $1/HQ
+    cp -R $1/*.jpg $tmp_dir 
 
-    echo "copying files to $tmp_dir"
-    cp -R $1/. $tmp_dir 
-    rm $1/* 
-    cd $tmp_dir
+    # rm $1/*.jpg 
+    cd $1/
+    pwd
     total=`ls -l . | egrep -c '^-'`
 
     for i in *.jpg; do
         new=$(printf "%02d.jpg" "$a")
         echo "[$a | $total]: Resizing $i to $new"
-        convert "$new[$2x]" "$1/$new"
+        # convert "$new[$2x]" "$new"
+        convert $i -resize 2800x2000 $new
         let a=a+1
     done
 }
